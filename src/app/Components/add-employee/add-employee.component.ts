a10db8edb147724a30923fb7d12b4b95ae414440
Aminul Islam <aminulcse2023@gmail.com>
2024-11-10 03:01:59 +0600

    About Component & standalone Componenet

---
+++ src/app/Components/add-employee/add-employee.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-employee',
+  standalone: true,
+  imports: [],
+  template:'<h1>Hello Admin</h1>', 
+  styleUrl: './add-employee.component.scss'
+})
+export class AddEmployeeComponent {
+
+}
